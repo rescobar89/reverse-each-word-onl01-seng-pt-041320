7de6006b0fcaf8e979d60ea530c516e0bed4fe69
Rocio X Escobar <rocio.x.escobar@gmail.com>
2020-04-22 07:58:32 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,6 +1,6 @@
 def reverse_each_word(sentence)
   words = sentence.split(" ")
   result = []
-  new_array = words.collect {|word| word.reverse }
-  new_array.join(" ")
+  result = words.collect {|word| word.reverse }
+  result.join(" ")
 end
